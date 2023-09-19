4c7beaa26c0f8b97ad8026df6dffa4801ee802c6
Emmanuel Mercado <emmanuel_mercado_99@hotmail.com>
2023-09-18 19:56:09 -0600

    Creado la base del servidor, se genera la API_KEY del servidor de los videojuegos

---
+++ api/index.js
@@ -0,0 +1,7 @@
+//Importar las dependencias
+const server = require('./src/app')
+
+
+server.listen(3001,()=>{
+    console.log('Servidor arriba en el puerto 3001');
+})
--- api/src/app.js
+++ api/src/app.js
@@ -1,1 +1,25 @@
+//Importar las dependencias
+const express = require('express')
+const server = express()
 
+//Midlewares
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header(
+       'Access-Control-Allow-Headers',
+       'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.header(
+       'Access-Control-Allow-Methods',
+       'GET, POST, OPTIONS, PUT, DELETE'
+    );
+    next();
+ });
+server.use(express.json())
+
+
+
+//Exportar el servidor
+
+module.exports = server;
