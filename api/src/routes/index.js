9f1985c9e335dcb2cdb083d9c79b8b373b73f590
Emmanuel Mercado <emmanuel_mercado_99@hotmail.com>
2023-09-19 12:36:37 -0600

    Al especificar un nombre ya manda los primero 15

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -13,11 +13,45 @@ routerVideogames.use(express.json())
 
 routerVideogames.get('/',async(req,res)=>{
     try{
-        const url = 'https://api.rawg.io/api/games?key='+API_KEY
+        let url = 'https://api.rawg.io/api/games?key='+API_KEY
+        const {name} = req.query
+        
+        if(name){
+            //https://api.rawg.io/api/games?search=Grand&key=f6eb52bcbcf14eb3b2670e43ac00abd8
+            url = 'https://api.rawg.io/api/games?search='+name+'&key='+API_KEY
+            await axios.get(url)
+            .then(response=>{
+                const gamesLimited = response.data.results.slice(0,15)
+                res.status(200).json(gamesLimited)
+            })
+            .catch(error =>{
+                res.status(500).json({ error: 'Error al hacer la solicitud a la API' });
+            })
+        }
+        else{
+            await axios.get(url)
+            .then(response=>{
+                res.json(response.data)
+            })
+            .catch(error =>{
+                res.status(500).json({ error: 'Error al hacer la solicitud a la API' });
+            })
+        }
 
-        axios.get(url)
+        
+
+    }
+    catch(error){
+    }
+})
+
+routerVideogames.get('/:idVideogame',async(req,res)=>{
+    try{
+        const {idVideogame} = req.params
+        const url = 'https://api.rawg.io/api/games/'+idVideogame+'?key='+API_KEY
+
+        await axios.get(url)
         .then(response=>{
-            console.log(response);
             res.json(response.data)
         })
         .catch(error =>{
@@ -28,5 +62,9 @@ routerVideogames.get('/',async(req,res)=>{
     catch(error){
     }
 })
+
+
+
+
 
 module.exports = routerVideogames;
---
+++ client/src/Components/CardVideogame/CardVideogame.jsx
@@ -0,0 +1,12 @@
+const CardVideogame = ()=>{
+    //ID auxiliar
+    const id = 3498
+    
+    return(
+        <div>
+            Card básica de juego
+        </div>
+    )
+}
+
+export default CardVideogame;
